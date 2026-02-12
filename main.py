fb73cb3693a0afa334e3ac78978ec27030add7a6
avuign <ant.vuign@gmail.com>
2026-02-12 14:09:33 +0100

    added rotation

--- main.py
+++ main.py
@@ -25,11 +25,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        dt = clock.tick(60) / 1000
+
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
 
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
